e61f82e5bd8d1243cdf0a5c5097e2f1379f79630
Lyubort Konstantin <lyubortk@gmail.com>
2018-10-03 21:37:51 +0300

    Tests added

--- Main.hs
+++ Main.hs
@@ -13,4 +13,6 @@ main = do
   runParser " 1 - 2 - 3 "
   runParser " (((9)))"
   runParser " 1 * 2 - 3 / 4 + 5"
-  runParser "var = 13 * 42"
+  runParser " var = 13 * 42"
+  runParser " a = -4"
+  runParser " var3 = a^34^(-3) * 3"
